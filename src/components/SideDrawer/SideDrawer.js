cd2c2594ee476ffa0361bee46f279166d302f8a2
DavidSalami <wizdave97@gmail.com>
2019-05-03 11:48:36 +0100

    started work on side drawer

---
+++ src/components/SideDrawer/SideDrawer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Paper } from '@material-ui/core';
+import { withStyles } from '@material-ui/core/styles';
+import styles from './styles';
+
+const sideDrawer = props => {
+    const { classes }= props;
+    return(
+        <Paper square={true}>
+
+        </Paper>
+    )
+}
