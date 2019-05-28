3d7262ad5405847b6b38732b98b1732db78f6497
DavidSalami <wizdave97@gmail.com>
2019-05-28 13:33:48 +0100

    Updates

---
+++ precache-manifest.65f31ab3fa630ddddbf92ebd63dd391a.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecf2639be2d0b4c5b3e487184c1d008f",
+    "url": "/electrostore/index.html"
+  },
+  {
+    "revision": "aedc3433e34dbdbd1704",
+    "url": "/electrostore/static/css/main.bc40ee40.chunk.css"
+  },
+  {
+    "revision": "3e54344c14a1ce9fcdd3",
+    "url": "/electrostore/static/js/10.088e0897.chunk.js"
+  },
+  {
+    "revision": "fdfcb5b3d67e2ba8d0b9",
+    "url": "/electrostore/static/js/2.0f27423e.chunk.js"
+  },
+  {
+    "revision": "1cb7f60be863ddef2c14",
+    "url": "/electrostore/static/js/3.d6616bfc.chunk.js"
+  },
+  {
+    "revision": "57cf497a2acd6f62b418",
+    "url": "/electrostore/static/js/4.23365e62.chunk.js"
+  },
+  {
+    "revision": "67f650ce7639a0d51277",
+    "url": "/electrostore/static/js/5.958f7619.chunk.js"
+  },
+  {
+    "revision": "15ad7596f50d3a45079a",
+    "url": "/electrostore/static/js/6.62cf7e03.chunk.js"
+  },
+  {
+    "revision": "ddb927dff595acddadea",
+    "url": "/electrostore/static/js/7.1006dffb.chunk.js"
+  },
+  {
+    "revision": "fc11c323df3bcea19082",
+    "url": "/electrostore/static/js/8.eb11f524.chunk.js"
+  },
+  {
+    "revision": "b578fea40e557266f4b2",
+    "url": "/electrostore/static/js/9.45b4178e.chunk.js"
+  },
+  {
+    "revision": "aedc3433e34dbdbd1704",
+    "url": "/electrostore/static/js/main.2c5c522b.chunk.js"
+  },
+  {
+    "revision": "1b4d06ee2b2118a8d9e7",
+    "url": "/electrostore/static/js/runtime~main.25611d9b.js"
+  },
+  {
+    "revision": "bafd50ce68c7de668be900a1e0d1e142",
+    "url": "/electrostore/static/media/Apple_logo.bafd50ce.svg"
+  },
+  {
+    "revision": "df32ef538428a3979bf136bd35ed9973",
+    "url": "/electrostore/static/media/Google_logo.df32ef53.svg"
+  },
+  {
+    "revision": "86dbe189647fc3e9fd76f50e7664a91b",
+    "url": "/electrostore/static/media/S10.86dbe189.jpg"
+  },
+  {
+    "revision": "da83d03e309442f1ce7fe6464ec60156",
+    "url": "/electrostore/static/media/Samsung_logo.da83d03e.svg"
+  },
+  {
+    "revision": "1516f332ad4f627173ff9664ce982b47",
+    "url": "/electrostore/static/media/algolia.1516f332.png"
+  },
+  {
+    "revision": "7810afe66ea95455941357b5ece75780",
+    "url": "/electrostore/static/media/iphoneX.7810afe6.jpg"
+  },
+  {
+    "revision": "508cecc6a265b492ae13a2a7e34834b7",
+    "url": "/electrostore/static/media/logo.508cecc6.svg"
+  }
+]);
